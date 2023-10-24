2e6ec777fa0af8ac71693877ef561e836ee8e5ee
Faisal Ahmed <faisalahmed8988@gmail.com>
2023-10-24 12:46:34 +0600

    .

---
+++ core-learning/golang/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+// const (
+// 	A int = 1
+// 	B = 3.14
+// 	C = "Hi!"
+//   )
+
+ //const A int = 1
+ 
+  
+
+func main() {
+	//a := 10;
+	//var a, b, c, d int = 1, 3, 5, 7;
+
+	// var k, v = 7, "hello" 
+	// c, d :=  78, "mama"
+	// fmt.Println("hello kaka kmn asen!")
+	// fmt.Println(k, v, c, d);				
+}   
