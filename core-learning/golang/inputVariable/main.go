0860b04b35099d7550fb232a0cb931bd19249b22
Faisal Ahmed <faisalahmed8988@gmail.com>
2023-11-05 15:20:17 +0600

    cses sheet one and two solved

--- core-learning/golang/inputVariable/main.go
+++ core-learning/golang/inputVariable/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func concat(s1 string, s2 string) string {
 	return s1 + s2
@@ -12,8 +14,16 @@ func main() {
 	test("Lane,", " happy birthday!")
 	test("Elon,", " hope that Tesla thing works out")
 	test("Go", " is fantastic")
+	hello()
+
+	fmt.Println("heello")
+	var key int = 233
 }
 
 func test(s1 string, s2 string) {
 	fmt.Println(concat(s1, s2))
+}
+
+func hello() {
+	fmt.Print("hello kaka kmn chole ")
 }
