d135a4940e53d8fbf0169403be13395f303422e4
Faisal Ahmed <faisalahmed8988@gmail.com>
2024-01-06 15:58:31 +0600

    pratice

--- core-learning/javascript/01_conversion.js
+++ core-learning/javascript/01_conversion.js
@@ -1,27 +1,39 @@
-function convertToNumber(value) {
-  if (value === null || value === undefined) {
-    return { status: "Value is null or undefined", number: null };
-  }
+// function convertToNumber(value) {
+//   if (value === null || value === undefined) {
+//     return { status: "Value is null or undefined", number: null };
+//   }
+//
+//   const number = Number(value);
+//
+//   if (isNaN(number)) {
+//     return { status: "Cannot be converted to a number", number: null };
+//   }
+//
+//   return { status: "Successful conversion", number: number };
+// }
 
-  const number = Number(value);
-
-  if (isNaN(number)) {
-    return { status: "Cannot be converted to a number", number: null };
-  }
-
-  return { status: "Successful conversion", number: number };
-}
+const { words } = require("lodash");
 
 let age = "33a";
 let temp = null;
 
+// const str = new String("hello world i am object"); // thing that its array [0]
+// console.log(str.valueOf());
+
+let numa = 34;
+
+const numaString = String(numa);
+
+// console.log(numaString);
+// console.log(typeof numaString);
+//
 const valueInNumber = Number(age);
 const tempInNumber = Number(temp);
 
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
-console.log(typeof tempInNumber);
-console.log(tempInNumber);
+// console.log(typeof tempInNumber);
+// console.log(tempInNumber);
 
 // 3 -> 3
 // "33" -> 33
--- core-learning/javascript/02_objectTricks.js
+++ core-learning/javascript/02_objectTricks.js
@@ -4,5 +4,10 @@ const course = {
   courseDuration: "6 month",
 };
 
+const arr = [1, 2, 3, 4, 5];
+const [a, b, c, ...rest] = arr;
+
+console.log(a, b);
+
 const { courseInstructor: Instructor } = course;
 console.log(Instructor);
