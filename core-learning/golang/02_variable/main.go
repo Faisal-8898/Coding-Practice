e1f376e312b76acadd0049404532310864f930fc
Faisal Ahmed <faisalahmed8988@gmail.com>
2024-01-22 18:30:15 +0600

    practice

---
+++ core-learning/golang/02_variable/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+
+	var str string = "ami go shikhtesi"
+	fmt.Println(str)
+	fmt.Printf("hello how are you: %T \n", str)
+	fmt.Printf("hello how are you: %T \n", str)
+}
