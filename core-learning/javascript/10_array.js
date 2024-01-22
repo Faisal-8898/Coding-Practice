fc4325574550141100e3b544f2e292ff4365f63f
Faisal Ahmed <faisalahmed8988@gmail.com>
2024-01-22 21:05:44 +0600

    practice_js

---
+++ core-learning/javascript/10_array.js
@@ -0,0 +1,5 @@
+// how to find min and max in arrray
+// const point = [4, 56, -1, 60];
+// point.sort((a, b) => a - b);
+// const min = point[0];
+// console.log(min);
---
+++ core-learning/javascript/11_math.js
@@ -0,0 +1,14 @@
+// console.log(Math.PI);
+// console.log(Math.sqrt(9));
+// console.log(Math.round(2.49));
+// console.log(Math.ceil(2.3));
+// console.log(Math.ceil(-2.3));
+// console.log(Math.floor(2.3));
+// console.log(Math.sign(31));
+// console.log(Math.pow(2, 3));
+// console.log(Math.abs(-44.5));
+// console.log(Math.min(23, 34, 56, 67));
+// const random = Math.random();
+// console.log(random);
+// const randombetween0_10 = Math.random() * 10;
+// console.log(randombetween0_10);
