a7a9d1aa8b67e1e2805522fabc781de09bf2ab4f
Faisal Ahmed <faisalahmed8988@gmail.com>
2024-03-10 23:35:46 +0600

    harkirat class work

--- React_course/frontend_todoApp/src/App.jsx
+++ React_course/frontend_todoApp/src/App.jsx
@@ -1,51 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todo, setTodo] = useState([
-    {
-      title: "My first todo",
-      description: "I love do somthing",
-      id: 123,
-    },
-    {
-      title: "want to go to gym",
-      description: "gym at 7 pm",
-      id: 987,
-    },
-    {
-      title: "codeem",
-      description: "short description",
-      id: 674,
-    },
-    {
-      title: "competitive programming",
-      description: "don't do it",
-      id: 987,
-    },
-  ]);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    console.log("Fetching data..");
+    fetch("http://localhost:3000/todos").then((response) => {
+      response.json().then((data) => {
+        setTodos(data);
+      });
+    });
+  }, []);
 
   return (
-    <>
-      {todo.map((todo) => {
+    <div>
+      {todos.map((todo) => {
         return (
-          <div>
-            <Todo title={todo.title} description={todo.description}></Todo>
-            <br />
-          </div>
+          <Todo
+            key={todo.id}
+            title={todo.title}
+            description={todo.description}
+          ></Todo>
         );
       })}
-    </>
+    </div>
   );
 }
 
 function Todo(props) {
   return (
-    <div style={{ background: "red" }}>
+    <div>
       {props.title}
+      <br />
       {props.description}
     </div>
   );
 }
-
 export default App;
