89274c0efff8e7ddb1ee5cddac35206d90845ed3
Faisal Ahmed <faisalahmed8988@gmail.com>
2024-03-22 07:12:13 +0600

    hello 2

--- problem_solving/cp:dsa/A_Nearly_Lucky_Number.cpp
+++ problem_solving/cp:dsa/A_Nearly_Lucky_Number.cpp
@@ -1,30 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    // Write C++ code here
-    long int n, r;
-    int count = 0;
-    cin >> n;
+int main() {
+  // Write C++ code here
+  long int n, r;
+  int count = 0;
+  cin >> n;
 
-    while (n > 0)
-    {
-        r = n % 10;
-        n = n / 10;
-        if (r == 7 || r == 4)
-        {
-            count++;
-        }
+  while (n > 0) {
+    r = n % 10;
+    n = n / 10;
+    if (r == 7 || r == 4) {
+      count++;
     }
-    if (count == 4 || count == 7)
-    {
-        cout << "YES";
-    }
-    else
-    {
-        cout << "NO";
-    }
+  }
+  if (count == 4 || count == 7) {
+    cout << "YES";
+  } else {
+    cout << "NO";
+  }
 
-    return 0;
+  return 0;
 }
